a73e90639c38464ba6282e45c9b8f6dae1c103ec
Srijan Shrestha <sshrestha@QKLY.io>
2021-08-05 22:36:36 +0545

    Workin on topbar and main section of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Topbar from "./components/topbar/Topbar";
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro"
+
 import "./App.scss";
 
 function App() {
   return (
     <div className="app">
-      HEllo
       <Topbar/>
       <div className="section">
-        
+        <Contact/>
       </div>
     </div>
   );
