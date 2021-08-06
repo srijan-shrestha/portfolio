47d1b377664f32fe4d44016fb35b8632e6ac038e
Srijan Shrestha <srijaniw@gmail.com>
2021-08-06 22:22:11 +0545

    Use unicons within header layout

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,45 @@ import "./topbar.scss"
 
 export default function Topbar() {
     return(
-        <div className="topbar">
-
-        </div>
+      <header className="header" id="header">
+          <nav className="nav container">
+            <a href="srijan" className="nav__logo">Srijan</a>
+            <div className="nav__menu" id="nav-menu">
+                <ul className="nav__list grid">
+                    <li className="nav__item">
+                        <a href="#home" className="nav__link">
+                            <i class="uil uil-estate"></i> Home
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#about" className="nav__link">
+                            <i class="uil uil-user"></i> About
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#skills" className="nav__link">
+                            <i class="uil uil-file-alt"></i>  Skills
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#services" className="nav__link">
+                            <i class="uil uil-briefcase-alt"></i> Services
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#portfolio" className="nav__link">
+                            <i class="uil uil-scenery"></i> Portfolio
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#contact" className="nav__link">
+                        <i class="uil uil-message"></i> Contact Me
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div>
+          </nav>
+      </header>
     )
 }
