ed66442ff5186d068c458f6342a553f6b9360514
Srijan Shrestha <srijaniw@gmail.com>
2022-01-17 22:05:41 +0545

    Work on topbar and main conatiner

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,23 @@
 export default function Main() {
     return (
         <main className="main">
-
+            <section className="home section" id="home">
+                <div className="home__conatiner container grid">
+                    <div className='home__content grid'>
+                        <div className="home__social">  
+                            <a href="https://www.linkedin.com/in/srijan-shrestha/" target="_blank" rel="noreferrer"className="home__social-icon">
+                                <i className='uil uil-linkedin-alt'></i>
+                            </a>
+                            <a href="https://github.com/srijan-shrestha" target="_blank" rel="noreferrer" className="home__social-icon">
+                            <i className='uil uil-github-alt'></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='home__img'>
+                        
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
