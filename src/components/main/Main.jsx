fce91f80a03387f155b89079afe782933533f6f3
Srijan Shrestha <srijaniw@gmail.com>
2022-01-21 13:34:51 +0545

    Work on blob svg and image in main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,10 @@
-import blobSvg from '../../assets/images/blob.svg'
+import blobImage from '../../assets/images/portfolio.jpg';
 
 const Main = () =>{
     return (
         <main className="main">
             <section className="home section" id="home">
-                <div className="home__conatiner container grid">
+                <div className="home__container container grid">
                     <div className='home__content grid'>
                         <div className="home__social">  
                             <a href="https://www.linkedin.com/in/srijan-shrestha/" target="_blank" rel="noreferrer"className="home__social-icon">
@@ -16,7 +16,29 @@ const Main = () =>{
                         </div>
                     </div>
                     <div className='home__img'>
-                        <img src={blobSvg} alt='svg'></img>
+                        <svg viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <mask id="mask0" mask-type="alpha">
+                                <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+                                130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+                                97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+                                0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+                            </mask>
+                            <g mask="url(#mask0)">
+                                <path d="M190.312 36.4879C206.  582 62.1187 201.309 102.826 182.328 134.186C163.346 
+                                165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+                                129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+                                -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+                                <image className='home__blob-img' xlinkHref={blobImage}/>
+                            </g>
+                        </svg>
+                    </div>
+                    <div className='home__data'>
+                        <h1 className='home__title'>Hi, I am Srijan</h1>
+                        <h3 className='home__subtitle'>Full Stack Developer</h3>
+                        <p className='home__description'>High Level experience in web design and development knowledge, producing quality work.</p>
+                        <a href='#contact' className='button button-flex'>
+                           Contact Me <i class="uil uil-message button__icon"></i>
+                        </a>
                     </div>
                 </div>
             </section>
@@ -26,3 +48,4 @@ const Main = () =>{
 
 export default Main;
 
+c
