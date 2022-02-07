74753f4fc8f7f4632a12034e30bdda7d0a25df92
Srijan Shrestha <srijaniw@gmail.com>
2022-02-07 23:51:34 +0545

    Work on intro and main component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,21 +4,6 @@ const Intro = () => {
   return (
     <section className='home section' id='home'>
       <div className='home__container container grid'>
-        <div className='home__content grid'>
-          <div className='home__social'>
-            <a
-              href='https://www.linkedin.com/in/srijan-shrestha/'
-              target='_blank'
-              rel='noreferrer'
-              className='home__social-icon'
-            >
-              <i className='uil uil-linkedin-alt'></i>
-            </a>
-            <a href='https://github.com/srijan-shrestha' target='_blank' rel='noreferrer' className='home__social-icon'>
-              <i className='uil uil-github-alt'></i>
-            </a>
-          </div>
-        </div>
         <div className='home__data'>
           <h1>Hi, I am</h1>
           <div>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,15 +9,60 @@ const Main = () => {
   return (
     <main className='main'>
       <div className='layout'>
-        <div className='layout__social'></div>
-        <div className='latout__content'>
-          <Intro />
-          <About />
-          <Skills />
-          <Experience />
-          <Contact />
+        <div className='layout__social'>
+          <ul className=''>
+            <li>
+              <a
+                href='https://www.linkedin.com/in/srijan-shrestha/'
+                target='_blank'
+                rel='noreferrer'
+                className='layout__social-icon'
+              >
+                <i className='uil uil-linkedin-alt'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://github.com/srijan-shrestha'
+                target='_blank'
+                rel='noreferrer'
+                className='layout__social-icon'
+              >
+                <i className='uil uil-github-alt'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://instagram.com/srijan_shresta'
+                target='_blank'
+                rel='noreferrer'
+                className='layout__social-icon'
+              >
+                <i className='uil uil-instagram-alt'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://twitter.com/piratesrijan'
+                target='_blank'
+                rel='noreferrer'
+                className='layout__social-icon'
+              >
+                <i className='uil uil-twitter-alt'></i>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className='layout__email'></div>
+        <div className='layout__content'>
+          <main className='layout__content__page'>
+            <Intro />
+            <About />
+            <Skills />
+            <Experience />
+            <Contact />
+          </main>
+        </div>
+        <div className='layout__email sideElem'></div>
       </div>
     </main>
   );
