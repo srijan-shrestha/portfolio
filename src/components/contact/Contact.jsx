af993fbd829cb53ae049d4cd4fbdd66b64f39231
Srijan Shrestha <srijaniw@gmail.com>
2022-02-08 16:11:32 +0545

    Work on contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,24 @@
-import "./contact.scss"
+import './contact.scss';
 
 export default function Contact() {
-    return (
-        <div className="contact">
+  return (
+    <section className='contact section grid' id='contact'>
+      <div className='contact__container container'>
+        <h2 className='contact__title '>Contact Me!</h2>
+
+        <div className='contact__purpose'>
+          <p>
+            I am always open to discuss about new project and opportunities. Whether you have something to ask or just
+            want to say "HI!".
+          </p>
+          <p>Feel free to reach out.</p>
+          <p className='contact__email'>srijans318@gmail.com</p>
         </div>
-    )
-} 
+
+        <div className='contact__button'>
+            <a className='button button-flex' href='mailto:srijans318@gmail.com'>Reach Out!</a>
+        </div>
+      </div>
+    </section>
+  );
+}
