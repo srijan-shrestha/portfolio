f79912ad9c093aaee551a20ce7c628d0bdc88de2
Srijan Shrestha <srijans318@gmail.com>
2023-07-02 23:13:40 -0400

    Update contect of about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,15 +39,28 @@ const About = () => (
       {/* <img src={aboutMeImage} alt='' className='about__img'></img> */}
       <div className='about__data'>
         <p className='about__description'>
-          I am a full-stack software engineer with a background in computer engineering. I started my journey in the
-          world of the web back in my university days and since then I am hooked on it.
+        I am a dedicated full-stack software engineer with a solid background in computer engineering. 
+        Since my university days, I've been fascinated by the world of web development, and that fascination has only deepened over time.
         </p>
         <p className='about__description'>
-          I strive to create applications that provide an intuitive user experience backed by robust and efficiently
-          functioning code. I have had the privilege of working at US-based companies and during that period I gained
-          extensive knowledge and years of experience, working in cutting edge web technologies while delivering quality
-          products and services.
+        Currently, I am based in Canada and working remotely for a Canadian software company. 
+        I specialize in creating user-friendly applications that provide a seamless and intuitive experience. 
+        With valuable experience gained from working with esteemed companies in the US, I've acquired extensive knowledge and expertise in cutting-edge web technologies. 
+        My goal is to consistently deliver high-quality products and services that meet the needs of users and clients.
         </p>
+
+        <p className='about__description'>
+        Beyond coding, I have a zest for life and enjoy various hobbies. 
+        I'm an avid football fan, finding joy and excitement on the field or watching it live. 
+        I also have a great love for nature, and you'll often find me exploring new hiking trails and marveling at the beauty of the outdoors. 
+        In my downtime, I appreciate the power of storytelling and enjoy watching movies that transport me to different worlds.
+        </p>
+
+        {/* <p className='about__description'>
+        Let's collaborate on bringing your ideas to life and creating exceptional digital experiences.
+        Whether it's building software or embracing the wonders of life, I'm always eager to embark on new adventures. 
+        Feel free to reach out, and let's discuss how we can work together to make a memorable impact!
+        </p> */}
 
         <div className='about__info'>
           <div className='about__info-container'>
